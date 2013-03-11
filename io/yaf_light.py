893fee0ef80bb484a97b70c2b965ce6f4441e455
povmaniaco <p.alcaide@hotmail.com>
2013-03-11 07:22:11 +0100

    Update syntax

--- io/yaf_light.py
+++ io/yaf_light.py
@@ -102,23 +102,22 @@ class yafLight:
 
         yi.printInfo("Exporting Lamp: {0} [{1}]".format(name, lampType))
 
-        if lamp.create_geometry and not self.lightMat:
+        if lamp.create_geometry:  # and not self.lightMat:
             yi.paramsClearAll()
             yi.paramsSetColor("color", color[0], color[1], color[2])  # color for spherelight and area light geometry
             yi.paramsSetString("type", "light_mat")
-            self.lightMat = self.yi.createMaterial("lm")
+            self.lightMat = self.yi.createMaterial(name)
             self.yi.paramsClearAll()
 
         if lampType == "point":
             yi.paramsSetString("type", "pointlight")
             if getattr(lamp, "use_sphere", False):
-                radius = max(lamp.distance, 0.01)  # avoid ZeroDivisionError
                 if lamp.create_geometry:
-                    ID = self.makeSphere(24, 48, pos[0], pos[1], pos[2], radius, self.lightMat)
+                    ID = self.makeSphere(24, 48, pos[0], pos[1], pos[2], lamp.yaf_sphere_radius, self.lightMat)
                     yi.paramsSetInt("object", ID)
                 yi.paramsSetString("type", "spherelight")
                 yi.paramsSetInt("samples", lamp.yaf_samples)
-                yi.paramsSetFloat("radius", radius)
+                yi.paramsSetFloat("radius", lamp.yaf_sphere_radius)
 
         elif lampType == "spot":
             if self.preview and name == "Lamp.002":
@@ -164,8 +163,8 @@ class yafLight:
             yi.paramsSetBool("soft_shadows", lamp.ies_soft_shadows)
 
         elif lampType == "area":
-            sizeX = 1.0
-            sizeY = 1.0
+            sizeX = lamp.size
+            sizeY = lamp.size_y
             matrix = lamp_object.matrix_world.copy()
 
             # generate an untransformed rectangle in the XY plane with
@@ -207,8 +206,8 @@ class yafLight:
             # "from" is not used for sunlight and infinite directional light
             yi.paramsSetPoint("from", pos[0], pos[1], pos[2])
         if lampType in {"point", "spot"}:
-            if getattr(lamp, "use_sphere", False):
-                power = 0.5 * power * power / (radius * radius)
+            if getattr(lamp, "use_sphere", False) and lampType == "point":
+                power = 0.5 * power * power / (lamp.yaf_sphere_radius * lamp.yaf_sphere_radius)
             else:
                 power = 0.5 * power * power
 
